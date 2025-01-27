9ed09bb4522b12235a77b225258b5dab728155b6
Cátia Silva <92873518+ecvcds@users.noreply.github.com>
2025-01-27 14:08:34 +0000

    setup - install dependencies and structure

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+
+  const handleClick = () => {
+    
+  }
+  return (
+    <>
+      <h1>FE-Exercise</h1>
+      <div className="card">
+       <button onClick={handleClick}>
+         Log in
+       </button>
+      </div>
+    </>
+  )
+}
+
+export default App
